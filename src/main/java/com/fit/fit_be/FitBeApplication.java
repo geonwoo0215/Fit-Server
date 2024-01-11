d0f6f934951956838ce8a1574a242dc85c67c826
geonwoo0215 <gw980215@naver.com>
2024-01-12 08:05:41 +0900

    feat : initial commit

---
+++ src/main/java/com/fit/fit_be/FitBeApplication.java
@@ -0,0 +1,13 @@
+package com.fit.fit_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FitBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fit/fit_be/FitBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fit.fit_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
