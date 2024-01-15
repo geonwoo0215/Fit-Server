86165c0bb0a0dd77f68e5ad479e3c814b26e7106
geonwoo0215 <gw980215@naver.com>
2024-01-16 02:43:07 +0900

    feat : S3 이미지 업로드 기능 추가

---
+++ src/main/java/com/fit/fit_be/global/aws/s3/controller/S3Controller.java
@@ -0,0 +1,33 @@
+package com.fit.fit_be.global.aws.s3.controller;
+
+import com.fit.fit_be.global.aws.s3.service.S3Service;
+import com.fit.fit_be.global.common.response.ApiResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Profile;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@Profile("!test")
+public class S3Controller {
+
+    private final S3Service s3service;
+
+    @PostMapping("/file/multiparty-files")
+    public ResponseEntity<ApiResponse<List<String>>> multipleFilesUpload
+            (
+                    @RequestPart List<MultipartFile> multipartFiles
+            ) {
+
+        List<String> uploadUrls = s3service.putObject(multipartFiles);
+        return new ResponseEntity<>(new ApiResponse<>(uploadUrls), HttpStatus.CREATED);
+    }
+}
+
